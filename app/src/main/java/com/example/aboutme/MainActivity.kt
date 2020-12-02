2a16adce4ad339e65121d3dabe7a2cbbc15e010c
swati <solankiswati71@gmail.com>
2020-12-02 13:20:39 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.aboutme
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val myName = MyName("Swati Solanki")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
+        binding.button.setOnClickListener {
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View) {
+        binding.apply {
+            myName?.nickName = binding.editTextTextPersonName.text.toString()
+            invalidateAll()
+            editTextTextPersonName.visibility = View.GONE
+            view.visibility = View.GONE
+            binding.nickNameText.visibility = View.VISIBLE
+        }
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
